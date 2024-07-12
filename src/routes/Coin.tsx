ee2bf3c07dce5b940b1950f3e0f6a35d5f923075
yhwba <yhwba@fount.co>
2024-07-12 15:41:37 +0900

    코드정리

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
    Switch,
    Route,
@@ -206,7 +205,7 @@ function Coin() {
                   </OverviewItem>
                   <OverviewItem>
                      <span>Price:</span>
-                     <span>{tickersData?.quotes.USD.price.toFixed(3)}</span>
+                     <span>{tickersData?.quotes?.USD.price.toFixed(3)}</span>
                   </OverviewItem>
                </Overview>
                <Description>{infoData?.description}</Description>
